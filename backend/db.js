a9b4eddaabfbe7ab7a405ff5792cb2411710f106
MinhQuan <minhquanygo@gmail.com>
2025-11-29 15:21:03 +0700

    ban tach rieng

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: '127.0.0.1', // Hoặc IP của database server
     user: 'root',      // Thay bằng user MySQL của bạn
-    password: '1234567890123wQ', // Thay bằng mật khẩu của bạn
+    password: 'Tbthsghj1357', // Thay bằng mật khẩu của bạn
     database: 'ssb', // Tên database của bạn
     port: 3307,
 });
